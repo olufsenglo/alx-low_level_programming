e532130cc83b4c60f88a9266e235cb4d216261ec
olufsenglo <olsheaolufsen@gmail.com>
2023-08-12 01:35:43 -0700

    Fixed new line bug

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,6 +11,6 @@ int main(void)
 	printf("Size of an int: %li byte(s)\n", sizeof(int));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 	printf("Size of an long long int: %zu byte(s)\n", sizeof(long long int));
-	printf("Size of a float: %li byte(s)\n", sizeof(float));
+	printf("Size of a float: %li byte(s)", sizeof(float));
 	return (0);
 }
