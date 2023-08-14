dcfb8a2f6ce23f78941f75c729e8f0b7d6551036
olufsenglo <olsheaolufsen@gmail.com>
2023-08-14 09:33:47 -0700

    Started task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char first_c = 'a';
+
+	while (first_c <= 'z')
+	{
+		if (first_c == 'q' || first_c == 'e')
+		{
+			first_c++;
+			continue;
+		}
+		
+		putchar(first_c);
+		first_c++;
+	}
+	putchar('\n');
+
+	return (0);
+}
