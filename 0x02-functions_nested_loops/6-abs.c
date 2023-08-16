7a76a3e6fcf7a8b3c741d7180d4ecf03cd4b795e
olufsenglo <olsheaolufsen@gmail.com>
2023-08-16 16:28:10 -0700

    fixed betty error

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _abs: computes the absolute value of an integer.
+ * _abs - computes the absolute value of an integer.
  * @n: number to test
  *
  * Return: absolute value of n
