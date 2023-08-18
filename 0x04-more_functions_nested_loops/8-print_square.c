301e5d5d5f2e703e9b1d6d96b1fd95eab95f85f8
olufsenglo <olsheaolufsen@gmail.com>
2023-08-18 16:29:46 -0700

    fixed logical error

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -11,15 +11,22 @@ void print_square(int size)
 {
 	int i = 0, j;
 
-	while (i < size)
+	if (size <= 0)
 	{
-		j = 0;
-		while (j < size)
-		{
-			_putchar('#');
-			j++;
-		}
 		_putchar('\n');
-		i++;
+	}
+	else
+	{
+		while (i < size)
+		{
+			j = 0;
+			while (j < size)
+			{
+				_putchar('#');
+				j++;
+			}
+			_putchar('\n');
+			i++;
+		}
 	}
 }
