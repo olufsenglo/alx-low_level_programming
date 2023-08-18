fdfd30efba291812301e7e766bea7571cfa9e1ed
olufsenglo <olsheaolufsen@gmail.com>
2023-08-18 16:18:28 -0700

    fixed logical error

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -11,21 +11,28 @@ void print_triangle(int size)
 {
 	int i = 0, j, k;
 
-	while (i < size)
+	if (size <= 0)
 	{
-		j = size - (i + 1);
-		while (j > 0)
-		{
-			_putchar(' ');
-			j--;
-		}
-		k = 0;
-		while (k <= i)
-		{
-			_putchar('#');
-			k++;
-		}
 		_putchar('\n');
-		i++;
+	}
+	else
+	{
+		while (i < size)
+		{
+			j = size - (i + 1);
+			while (j > 0)
+			{
+				_putchar(' ');
+				j--;
+			}
+			k = 0;
+			while (k <= i)
+			{
+				_putchar('#');
+				k++;
+			}
+			_putchar('\n');
+			i++;
+		}
 	}
 }
