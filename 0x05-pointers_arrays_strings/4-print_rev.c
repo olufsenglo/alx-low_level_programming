93ed75f9d136d2c947ac944a7c30bb8aae6f76b0
olufsenglo <olsheaolufsen@gmail.com>
2023-08-22 16:10:13 -0700

    finised task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse
+ * @s: string
+ *
+ * Return: Always 0.
+ */
+void print_rev(char *s)
+{
+	int length = 0;
+	int i = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	for (i = length - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+
+	_putchar('\n');
+}
