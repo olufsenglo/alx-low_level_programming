19dd3540ce4a74cb8180a50ebf30aa368a03dc02
olufsenglo <olsheaolufsen@gmail.com>
2023-08-26 09:01:54 -0700

    fixed betty error

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * string_toupper: changes all lowercase letters of a string to uppercase
+ * string_toupper - changes all lowercase letters of a string to uppercase
  * @c: character
  *
  * Return: string
