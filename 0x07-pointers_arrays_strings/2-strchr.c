cd3ad0b3462fe98a2d886ec0dc55f4f4d542c8a1
olufsenglo <olsheaolufsen@gmail.com>
2023-08-29 04:02:16 -0700

    started task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: source
+ * @c: character
+ *
+ * Return: a pointer to dest
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+	
+	/*can use *s but I currently have a better understanding of s[i]*/
+	/* planning on switching to *s */
+	while (s[i] != '\0')
+	{
+		if (s[i] == c)
+		{
+			/*xter found, return its adrs*/
+			return &s[i];
+		}
+		i++;
+	}
+
+	/*if ch is '\0' return pointer to '\0'*/
+	if (c == '\0')
+		return &s[i];
+
+	/*character not found */
+	return NULL;
+}
