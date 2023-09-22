19ab62248ae47a913bf9ca6203213e303c8e430a
olufsenglo <olsheaolufsen@gmail.com>
2023-09-21 19:56:27 -0700

    Holberton singly list task

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * free_list - frees linked list
+ * @head: list
+ *
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
