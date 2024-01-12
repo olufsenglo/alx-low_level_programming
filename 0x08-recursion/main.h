6c33d87f5bc73a726b0ece82e09f383baad26b69
Olshea <olsheaolufsen@gmail.com>
2024-01-11 17:37:58 -0800

    1. Why is it so important to dream? Because, in my dreams we are together

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,10 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Declaration of the _putchar function */
 int _putchar(char c);
 
-/* Declaration of the _puts_recursion function */
 void _puts_recursion(char *s);
+void _print_rev_recursion(char *s);
 
 #endif /* MAIN_H */
